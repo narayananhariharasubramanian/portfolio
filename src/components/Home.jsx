0ab28ab26513c960592d6fcf17aa03f39c6f68df
Narayanan H <161689056+narayananhariharasubramanian@users.noreply.github.com>
2024-05-02 11:40:48 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,7 @@ export default function Home(){
     <div className='max-w-[1000px] mx-auto flex flex-col justify-center h-full'>
         <h1 className='text-[#dae3eb] text-3xl sm:text-5xl font-bold ml-3'>Narayanan H</h1>
         <h2 className="text-2xl my-1 text-[#bac6d1] ml-3">Full Stack Developer</h2>
-        <p className="w-[75%] mt-2 text-[#bac6d1] ml-3">A software developer with 17 years of experience, very passionate about technologies, 2 years of experience in ReactJS, HTML, CSS, TailwindCSS, NodeJS.
+        <p className="w-[75%] mt-2 text-[#bac6d1] ml-3">A software developer with 17 years of experience, very passionate about technologies, 5 years of experience in ReactJS, HTML, CSS, TailwindCSS, NodeJS.
         Looking for a career that challenges me always.</p>
         <p className="w-[75%] mt-2 text-[#bac6d1] ml-3">This page is built using <b>ReactJS, HTML and TailwindCSS</b></p>
     </div></div>)
